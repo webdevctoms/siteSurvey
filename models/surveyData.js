e5ead1274cd118b66b7b44e6302a38332ab1df3a
Michael Peterson <WebDev@ctoms.ca>
2018-11-29 13:45:42 -0700

    finished survey model added starter router files

---
+++ models/surveyData.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const surveySchema = mongoose.Schema({
+	uid: {type:Number},
+	questions: {type:Array},
+	answers: {type:Array},
+	email: {type:String}
+});
+
+surveySchema.methods.serialize = function(){
+	return{
+		questions:this.questions
+	}
+};
+
+const SurveyData = mongoose.model("SurveyData",surveySchema);
+
+module.exports = {SurveyData};
